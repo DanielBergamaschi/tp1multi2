9a30c895cbb2bd9b49a8778cf63f6673cd9f52de
DanielBergamaschi <danielbergamaschi2021@gmail.com>
2026-05-10 19:13:09 -0300

    agrego carpetas de fuentes y audio, modifico tmb js de logia para probar cosas, todavia en proceso

--- js/pagina4-logia.js
+++ js/pagina4-logia.js
@@ -1,14 +1,34 @@
 let curvaVelo1 = 0;
 let curvaVelo2 = 0;
 
+let curvaVelo1Y = 0;
+let curvaVelo2Y = 0;
+
 let bandera1 = true;
 let bandera2 = true;
+let bandera3 = true;
+let bandera4 = true;
+let bandera5 = true;
+let bandera6 = true;
+
+let cuadroTextoX = 0;
+let cuadroTextoY = 0;
+
+let ledLightFuente;
+
+function preload () {
 
 
+    ledLightFuente = loadFont('fuentes/ledlight.otf')
+
+
+}
 
 function setup() {
     let canvas = createCanvas(300, 480);
     canvas.parent('canvasDiv');
+
+
 
  // palacio_barolo.filter(THRESHOLD, 0.5);
   
@@ -18,34 +38,53 @@ function draw() {
   background(0, 20);
   frameRate (20);
   
+
+    
     push ();
         noFill ();
         stroke (150, 150);
         strokeWeight (1);
         veloMaya ();
     pop();
-  
+
+
 }
 
 
 function movimientoVelo () {
 
-        if (curvaVelo1 <= 300 && bandera1 === true) 
+        if (curvaVelo1 <= 180 && bandera1 === true) 
         {curvaVelo1 += 2*1.50} 
-        else if (curvaVelo1 >= 0 && bandera1 === true) 
+        else if (curvaVelo1 >= 120 && bandera1 === true) 
         {bandera1 = false}
-        else if (curvaVelo1 > 0 && bandera1 === false)
+        else if (curvaVelo1 > 120 && bandera1 === false)
         {curvaVelo1 -= 2*1.50}
-        else if (curvaVelo1 == 0 && bandera1 === false) {bandera1 = true}
+        else if (curvaVelo1 == 120 && bandera1 === false) {bandera1 = true}
 
-        if (curvaVelo2 <= 300 && bandera2 === true) 
+        if (curvaVelo2 <= 180 && bandera2 === true) 
         {curvaVelo2 += 2*1.25} 
-        else if (curvaVelo2 >= 0 && bandera2 === true) 
+        else if (curvaVelo2 >= 120 && bandera2 === true) 
         {bandera2 = false}
-        else if (curvaVelo2 > 0 && bandera2 === false)
+        else if (curvaVelo2 > 120 && bandera2 === false)
         {curvaVelo2 -= 2*1.25}
-        else if (curvaVelo2 == 0 && bandera2 === false) {bandera2 = true}
-    }
+        else if (curvaVelo2 == 120 && bandera2 === false) {bandera2 = true}
+
+         if (curvaVelo1Y <= 480 && bandera3 === true) 
+        {curvaVelo1Y += 2*1.25} 
+        else if (curvaVelo1Y >= 0 && bandera3 === true) 
+        {bandera3 = false}
+        else if (curvaVelo1Y > 0 && bandera3 === false)
+        {curvaVelo1Y -= 2*1.25}
+        else if (curvaVelo1Y == 0 && bandera3 === false) {bandera3 = true}
+
+         if (curvaVelo2Y <= 480 && bandera4 === true) 
+        {curvaVelo2Y += 2*1.25} 
+        else if (curvaVelo2Y >= 0 && bandera4 === true) 
+        {bandera4 = false}
+        else if (curvaVelo2Y > 0 && bandera4 === false)
+        {curvaVelo2Y -= 2*1.25}
+        else if (curvaVelo2Y == 0 && bandera4 === false) {bandera4 = true}
+}
 
 function veloMaya () {
 
@@ -53,10 +92,57 @@ function veloMaya () {
 
     for (i = 0; i < 300; i += 10) {
 
-        bezier (i+random (1, 5), 0, curvaVelo1, 0, curvaVelo2, 0, i-random (1,5), 480)
+        bezier (i+random (1, 5), 0, curvaVelo1, curvaVelo1Y, curvaVelo2, curvaVelo2Y, i-random (1,5), 480)
 
 
     }
+
+
+}
+
+function cajaTexto () {
+
+        /*if (cuadroTextoX <= 100 && bandera5 === true) 
+        {cuadroTextoX += 2*1.25} 
+        else if (cuadroTextoX >= 0 && bandera5 === true) 
+        {bandera5 = false}
+        else if (cuadroTextoX > 0 && bandera5 === false)
+        {cuadroTextoX -= 2*1.25}
+        else if (cuadroTextoX == 0 && bandera5 === false) {bandera5 = true}
+
+
+         if (cuadroTextoY <= 300 && bandera6 === true) 
+        {cuadroTextoY += 2*1.25} 
+        else if (cuadroTextoY >= 0 && bandera6 === true) 
+        {bandera6 = false}
+        else if (cuadroTextoY > 0 && bandera6 === false)
+        {cuadroTextoY -= 2*1.25}
+        else if (cuadroTextoY == 0 && bandera6 === false) {bandera6 = true}*/
+
+        cuadroTextoX = random (50, 100);
+        cuadroTextoY = random (50, 300);
+
+
+}
+
+function cuadradosTexto () {
+
+    cajaTexto ();
+
+    push();
+        stroke (255, 50);
+        strokeWeight (1);
+        fill (0, 20);
+        rect (15, 20, cuadroTextoX+10, cuadroTextoY);
+    pop();
+
+    push();
+        fill (255, 20);
+        textFont (ledLightFuente);
+        textSize (15);
+        text ("El Eden esta bajo nuestros pies dijeron los rebeldes y con ese grito degollaron a los que escuchaban las voces del cielo", 20, 20, cuadroTextoX, cuadroTextoY);
+    pop();
+
 
 
 }
