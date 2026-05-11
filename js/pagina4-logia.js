b6b8e4c6996363cc1d1679cbd9bda83e40a29fc8
DanielBergamaschi <danielbergamaschi2021@gmail.com>
2026-05-10 23:38:18 -0300

    agregue imagenes para pagina4, segui experimentando con js, probando estetica

--- js/pagina4-logia.js
+++ js/pagina4-logia.js
@@ -16,10 +16,27 @@ let cuadroTextoY = 0;
 
 let ledLightFuente;
 
+let imagen1;
+let imagen2;
+let imagen3;
+let imagen4;
+let imagen5;
+
+let galeria;
+
+
+
 function preload () {
 
 
-    ledLightFuente = loadFont('fuentes/ledlight.otf')
+    ledLightFuente = loadFont('fuentes/ledlight.otf');
+    imagen1 = loadImage ("img/pagina4_logia/logia5.jpg");
+    imagen2 = loadImage ("img/pagina4_logia/logia6.jpg");
+    imagen3 = loadImage ("img/pagina4_logia/logia7.jpg");
+    imagen4 = loadImage ("img/pagina4_logia/logia8.jpg");
+    imagen5 = loadImage ("img/pagina4_logia/logia9.jpg");
+
+
 
 
 }
@@ -30,16 +47,20 @@ function setup() {
 
 
 
- // palacio_barolo.filter(THRESHOLD, 0.5);
   
 }
 
 function draw() {
   background(0, 20);
   frameRate (20);
-  
 
-    
+    crearGaleria ();
+
+  
+   imagen1Funcion ();
+
+   cuadradosTexto ();
+
     push ();
         noFill ();
         stroke (150, 150);
@@ -54,19 +75,19 @@ function draw() {
 function movimientoVelo () {
 
         if (curvaVelo1 <= 180 && bandera1 === true) 
-        {curvaVelo1 += 2*1.50} 
+        {curvaVelo1 += 2*0.50} 
         else if (curvaVelo1 >= 120 && bandera1 === true) 
         {bandera1 = false}
         else if (curvaVelo1 > 120 && bandera1 === false)
-        {curvaVelo1 -= 2*1.50}
+        {curvaVelo1 -= 2*0.50}
         else if (curvaVelo1 == 120 && bandera1 === false) {bandera1 = true}
 
         if (curvaVelo2 <= 180 && bandera2 === true) 
-        {curvaVelo2 += 2*1.25} 
+        {curvaVelo2 += 2*0.25} 
         else if (curvaVelo2 >= 120 && bandera2 === true) 
         {bandera2 = false}
         else if (curvaVelo2 > 120 && bandera2 === false)
-        {curvaVelo2 -= 2*1.25}
+        {curvaVelo2 -= 2*0.25}
         else if (curvaVelo2 == 120 && bandera2 === false) {bandera2 = true}
 
          if (curvaVelo1Y <= 480 && bandera3 === true) 
@@ -119,8 +140,8 @@ function cajaTexto () {
         {cuadroTextoY -= 2*1.25}
         else if (cuadroTextoY == 0 && bandera6 === false) {bandera6 = true}*/
 
-        cuadroTextoX = random (50, 100);
-        cuadroTextoY = random (50, 300);
+        cuadroTextoX = random (0, 300);
+        cuadroTextoY = random (0, 480);
 
 
 }
@@ -133,16 +154,44 @@ function cuadradosTexto () {
         stroke (255, 50);
         strokeWeight (1);
         fill (0, 20);
-        rect (15, 20, cuadroTextoX+10, cuadroTextoY);
+        rect (cuadroTextoX-5, cuadroTextoY-15, cuadroTextoX+20, cuadroTextoY+10);
     pop();
 
     push();
-        fill (255, 20);
+        fill (255, 100);
         textFont (ledLightFuente);
-        textSize (15);
-        text ("El Eden esta bajo nuestros pies dijeron los rebeldes y con ese grito degollaron a los que escuchaban las voces del cielo", 20, 20, cuadroTextoX, cuadroTextoY);
+        textSize (12);
+        text ("El Eden esta bajo nuestros pies dijeron los rebeldes y con ese grito degollaron a los que escuchaban las voces del cielo", cuadroTextoX, cuadroTextoY, cuadroTextoX, cuadroTextoY);
     pop();
 
 
+
+}
+
+
+function imagen1Funcion () {
+
+    push();
+        tint (255, 20);
+        for (i = 0; i < 5; i += 1) {
+            image (galeria[i].imagen, galeria[i].posicionX, galeria[i].posicionY, galeria[i].ancho/4, galeria[i].largo/4);
+        }
+    pop();
+
+
+}
+
+
+function crearGaleria () {
+
+    galeria = [
+        
+        {imagen:imagen1, ancho:613, largo:261, posicionX: random (15, 20), posicionY: random (15, 20),},
+        {imagen:imagen2, ancho:156, largo:562, posicionX: random (95, 100), posicionY: 360,},
+        {imagen:imagen3, ancho:708, largo:198, posicionX: random (0, 5), posicionY: random (195, 200),},
+        {imagen:imagen4, ancho:72, largo:324, posicionX: random (275, 280), posicionY: 100,},
+        {imagen:imagen5, ancho:189, largo:528, posicionX: random (0, 10), posicionY: random (295, 300),},
+
+    ]
 
 }
